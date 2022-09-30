28e52dfabe2862644573b574f6e74190f36623e7
artenlf <felipe.arten@gmail.com>
2022-09-29 23:16:21 -0300

    adds Home footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import Logo from "../../Logo";
+import Wrapper from "../Wrapper";
+
+const FooterSection = styled.section`
+  align-items: center;
+  background-color: #eaf2fd;
+  display: flex;
+  justify-content: center;
+  height: 470px;
+  position: relative;
+  top: 602px;
+  width: 100vw;
+  z-index: 1;
+`;
+
+const FooterContainer = styled.div`
+  height: 240px;
+  width: 1136px;
+`;
+
+const FooterDeveloper = styled.p`
+  align-items: center;
+  color: #464646;
+  font-size: 16px;
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  height: 102px;
+  margin: 0;
+  position: relative;
+  top: 602px;
+  width: 100vw;
+  z-index: 1;
+`;
+
+const Footer = () => {
+  return (
+    <>
+      <FooterSection>
+        <FooterContainer>
+          <Logo />
+        </FooterContainer>
+      </FooterSection>
+      <FooterDeveloper>Desenvolvido por artenlf 2022</FooterDeveloper>
+    </>
+  );
+};
+
+export default Footer;
