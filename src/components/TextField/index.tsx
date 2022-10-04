37d2b41edf30ae1584b6a8a07c2414d7131b9edd
artenlf <felipe.arten@gmail.com>
2022-10-04 18:37:46 -0300

    Adds TextField component

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import TextField from "@mui/material/TextField";
+
+const TextFieldInput = styled(TextField)`
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 2px 2px 4px #0000001f;
+  margin: 8px auto;
+  width: 560px;
+`;
+
+export default TextFieldInput;
