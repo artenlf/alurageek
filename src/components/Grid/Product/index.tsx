6b9220420f74982422dc120808af504f6ae2be1b
artenlf <felipe.arten@gmail.com>
2022-10-04 07:45:04 -0300

    Refactors products grid

--- src/components/Grid/Product/index.tsx
+++ src/components/Grid/Product/index.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Image from "next/image";
-import product from "./product.json";
+import product from "./products.json";
 
 type Props = typeof product[0];
 
--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,40 +1,36 @@
-import styled from "styled-components";
-import Wrapper from "../Wrapper";
 import Product from "./Product";
 import TitleRow from "./TitleRow";
-import product from "./Product/product.json";
+import product from "./Product/products.json";
+import GridContainer from "./style";
+import { Fragment } from "react";
 
 type Props = typeof product[0];
 
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  height: 100%;
-  margin: 0 auto 64px;
-  width: 1136px;
-  z-index: 2;
-`;
-
 let listOfCategories = product.map((item) => item.category);
-listOfCategories = listOfCategories.filter((v, i) => listOfCategories.indexOf(v) === i);
+listOfCategories = listOfCategories.filter(
+  (v, i) => listOfCategories.indexOf(v) === i
+);
 
 const Grid = () => {
   return (
     <GridContainer>
-      <>
-        {listOfCategories.map((cat, index) => (
-          <>
-            <TitleRow key={index} category={cat} name={""} price={0} picture={""} id={0} />
-            <>
-              {product
-                .filter((item) => item.category === cat)
-                .map((item, index) => (
-                  <Product key={index} id={item.id} name={item.name} price={item.price} picture={item.picture} category={item.category} />
-                ))}
-            </>
-          </>
-        ))}
-      </>
+      {listOfCategories.map((cat, index) => (
+        <Fragment key={index}>
+          <TitleRow category={cat} />
+          {product
+            .filter((item) => item.category === cat)
+            .map((item) => (
+              <Product
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                picture={item.picture}
+                category={item.category}
+              />
+            ))}
+        </Fragment>
+      ))}
     </GridContainer>
   );
 };
