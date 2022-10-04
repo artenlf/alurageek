e89f3d8b053148f16468f73dd733ffd9a3316354
artenlf <felipe.arten@gmail.com>
2022-10-04 18:38:15 -0300

    adds admin page

---
+++ src/pages/admin.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import NewProductForm from "../components/NewProductForm";
+
+const Admin: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>AluraGeek | Admin</title>
+      </Head>
+      <Header />
+      <NewProductForm />
+      <Footer />
+    </>
+  );
+};
+
+export default Admin;
