82c8ec78ae81ebccce827ea9685e044a812cbc7f
artenlf <felipe.arten@gmail.com>
2022-10-04 18:53:35 -0300

    fix Title

--- src/components/NewProductForm/index.tsx
+++ src/components/NewProductForm/index.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Title from "../../Title";
+import Title from "../Title";
 import Button from "../Button";
 import TextFieldInput from "../TextField";
 
