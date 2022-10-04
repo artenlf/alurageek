83f6e2e15fb9b9428546eae5f2eb435e4cf042ed
artenlf <felipe.arten@gmail.com>
2022-10-04 18:31:26 -0300

    adds type for buttons

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 interface Props {
   primary: boolean;
   formSubmit: boolean;
+  fullWidth: boolean;
 }
 
 const Button = styled.button<Props>`
@@ -17,7 +18,14 @@ const Button = styled.button<Props>`
   justify-content: center;
   height: 51px;
   transition: 0.3s;
-  width: ${(p) => (p.primary ? "182px" : p.formSubmit ? "182px" : "130px")};
+  width: ${(p) =>
+    p.primary
+      ? "182px"
+      : p.fullWidth
+      ? "560px"
+      : p.formSubmit
+      ? "182px"
+      : "130px"};
   :hover {
     cursor: pointer;
     font-weight: 600;
