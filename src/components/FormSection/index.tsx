d26dc5917d3c4e6a01be880404035a508f57c2be
artenlf <felipe.arten@gmail.com>
2022-10-05 16:52:20 -0300

    adds a form section component

---
+++ src/components/FormSection/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const FormSection = styled.section`
+  display: flex;
+  justify-content: center;
+  background-color: #f5f5f5;
+  width: 100%;
+`;
+
+export default FormSection;
