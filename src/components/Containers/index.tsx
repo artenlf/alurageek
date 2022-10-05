58d2874af63d946ae66cd40cddc44b58e4892f24
artenlf <felipe.arten@gmail.com>
2022-10-05 16:51:35 -0300

    adds containers components

---
+++ src/components/Containers/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 32px;
+  margin-bottom: 64px;
+`;
+
+export default FormContainer;
