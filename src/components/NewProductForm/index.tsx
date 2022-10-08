97db514c9dbf02f9e0b030de8b2a1c8d6db83a12
artenlf <felipe.arten@gmail.com>
2022-10-07 22:34:58 -0300

    adds upload and selector menu for categories

--- src/components/NewProductForm/index.tsx
+++ src/components/NewProductForm/index.tsx
@@ -1,31 +1,45 @@
+import React from "react";
 import { Title } from "../Title";
 import Button from "../Button";
-import TextFieldInput from "../TextField";
+import TextFieldInput from "../TextField/styles";
 import GreySection from "../GreySection";
 import FormContainer from "../Containers/FormContainer";
-
-const formLabels = [
-  "URL da imagem",
-  "Categoria",
-  "Nome do Produto",
-  "Preço do Produto",
-];
+import UploadButton from "../UploadButton";
+import { InlineWrapper } from "../Wrapper";
+import { listOfCategories } from "../ProductsGrids";
+import { Menu, MenuItem } from "@mui/material";
 
 const NewProductForm = () => {
+  const [category, setCategory] = React.useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCategory(event.target.value);
+  };
   return (
     <>
       <GreySection>
         <FormContainer>
           <Title>Adicionar novo produto</Title>
-          {formLabels.map((label, index) => (
-            <TextFieldInput key={index} label={label} required />
-          ))}
+          <InlineWrapper>
+            <TextFieldInput label="URL da imagem" fullWidth />
+            <UploadButton />
+          </InlineWrapper>
           <TextFieldInput
-            label="Descrição do Produto"
-            required
-            multiline
-            minRows={3}
-          />
+            select
+            label="Categoria"
+            value={category}
+            onChange={handleChange}
+            fullWidth
+          >
+            {listOfCategories.map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </TextFieldInput>
+          <TextFieldInput label="Nome do Produto" fullWidth />
+          <TextFieldInput label="Preço do Produto" fullWidth />
+          <TextFieldInput label="Descrição do produto" minRows={3} fullWidth />
 
           <Button
             primary={false}
