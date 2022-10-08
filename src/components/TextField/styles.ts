26c944099e120eb265874413a8817662f59d2b48
artenlf <felipe.arten@gmail.com>
2022-10-07 22:34:02 -0300

    refactors and adds functions to TextField

--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -1,12 +1,11 @@
+import { TextField } from "@mui/material";
 import styled from "styled-components";
-import TextField from "@mui/material/TextField";
 
 const TextFieldInput = styled(TextField)`
   background-color: white;
   border-radius: 4px;
   box-shadow: 2px 2px 4px #0000001f;
   margin: 8px auto;
-  width: 560px;
 `;
 
 export default TextFieldInput;
