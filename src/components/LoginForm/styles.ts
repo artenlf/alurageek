5810488efcfdc3fcd0501752d1b66a7daa040754
artenlf <felipe.arten@gmail.com>
2022-10-11 11:07:32 -0300

    fix login form

--- src/components/LoginForm/styles.ts
+++ src/components/LoginForm/styles.ts
@@ -1,11 +1,10 @@
 import styled from "styled-components";
-import FormContainer from "../NewProductForm/styles";
+import { FormContainer } from "../NewProductForm/styles";
 
 const LoginFormContainer = styled(FormContainer)`
   justify-content: space-between;
   height: 291px;
   margin: 91px auto;
-  width: 423px;
 `;
 
 export default LoginFormContainer;
